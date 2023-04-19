65aa82d8a63965e50fca9fb6105878fd23df8de4
ikaslea <ikaslea@INF200448.zornotza.local>
2023-04-19 10:18:26 +0200

    Ficheros logs 2

---
+++ Ethazi/src/erronka/Formato.java
@@ -0,0 +1,15 @@
+package erronka;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class Formato extends Formatter {
+
+	@Override
+	public String format(LogRecord record) {
+		// TODO Auto-generated method stub
+		return "<Mezua>" + record.getMessage() + "</Mezua>\n" + "<Maila>" + record.getLevel() + "</Maila>\n"
+				+ "<Klasea>" + record.getSourceClassName() + "</Klasea>";
+	}
+
+}
