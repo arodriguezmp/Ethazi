e6928921030347ba7516cfe60daefaaf09834738
ikaslea <ikaslea@INF200448.zornotza.local>
2023-04-28 14:52:58 +0200

    Erreserba test

--- Ethazi/src/erronka/ErreserbaTest.java
+++ Ethazi/src/erronka/ErreserbaTest.java
@@ -13,9 +13,9 @@ class ErreserbaTest {
 	 */
 	@Test
 	public void testConstructor() {
-		Erreserba erreserba = new Erreserba("12345678A", 100, new Date(), new Date(), 1);
-		assertEquals(100, erreserba.getPrezioa());
-		assertEquals("12345678A", erreserba.getNan());
-		assertEquals(1, erreserba.getKod_ostatua());
+		Erreserba erreserba = new Erreserba("11111111A", 100, new Date(), new Date(), 1);
+
+		assertEquals("11111111A", erreserba.getNan());
+
 	}
 }
